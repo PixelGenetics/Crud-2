50c085146ba0f366f9ae997193debc488e8f41be
Kevin Antonio Borge Gutierrez <108099077+PixelGenetics@users.noreply.github.com>
2024-02-13 18:43:17 -0600

    Changing style.

--- src/Card/CrudGet.jsx
+++ src/Card/CrudGet.jsx
@@ -41,17 +41,17 @@ const CrudGet = () => {
                     <span>CUMPLEAÑOS</span>
                     <p>{user.birthday}</p>
                     <button onClick={() => deleteApi('/users', user.id)}>Borrar</button>
-                    <button onClick={() => handleUpdate(user.id)}>Update</button>
-                    
-                    {openUserId === user.id && ( // Mostrar el formulario solo para el usuario seleccionado
+                    <button onClick={() => handleUpdate(user.id)}>Actualizar</button>
+    
+                    {openUserId === user.id && (
                         <div className="modal">
                             <div className="modal-content">
                                 <h2>Actualizar Usuario</h2>
                                 <label htmlFor="email">Nuevo email:</label>
                                 <input id="email" type="email" name="email" onChange={handleChange} />
-                                <label htmlFor="first_name">Nuevo first name:</label>
+                                <label htmlFor="first_name">Nuevo Nombre:</label>
                                 <input id="first_name" type="text" name="first_name" onChange={handleChange} />
-                                <label htmlFor="last_name">Nuevo last name:</label>
+                                <label htmlFor="last_name">Nuevo Apellido:</label>
                                 <input id="last_name" type="text" name="last_name" onChange={handleChange} />
                                 <label htmlFor="birthday">Nueva fecha de nacimiento:</label>
                                 <input id="birthday" type="date" name="birthday" onChange={handleChange} />
